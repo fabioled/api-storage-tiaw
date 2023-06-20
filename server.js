30794ce0f598b5001f700265ee82ff2889cfe6de
Fábio Cordeiro <fabioled@gmail.com>
2023-06-20 13:47:19 -0300

    Deploy no Vercel

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-const jsonServer = require('json-server');
+// JSON Server module
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); 
+const router = jsonServer.router("db/db.json");
+
+// Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; 
 
 server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/api/*": "/$1",
+ })
+);
 server.use(router);
+// Listen to port
+server.listen(3000, () => {
+ console.log("JSON Server is running");
+});
 
-server.listen(port);
+// Export the Server API
+module.exports = server;
